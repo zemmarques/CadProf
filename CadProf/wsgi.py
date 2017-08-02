52facca45810d8f7130332bf6df197601286cbbf
zemmarques <zemaile@gmail.com>
2017-08-02 10:30:02 +0100

    inicial commit of the project

---
+++ CadProf/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CadProf project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CadProf.settings")
+
+application = get_wsgi_application()
